22d18a7952b79263d129fad986c27c66ad8176d4
yaotutu <957810668@qq.com>
2024-07-08 16:10:40 +0800

    feat: Add xml-formatter dependency for XML formatting
    This commit adds the xml-formatter dependency to the backend package.json file. The xml-formatter library is used for formatting XML data in a human-readable way. This will improve the readability and maintainability of XML templates used in the project.
    
    Co-authored-by: yaotutu

--- packages/backend/src/common/rss-parser/feed-generator.service.ts
+++ packages/backend/src/common/rss-parser/feed-generator.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as Handlebars from 'handlebars';
+const xmlFormat = require('xml-formatter').default || require('xml-formatter');
 
 @Injectable()
 export class FeedGeneratorService {
@@ -11,15 +12,7 @@ export class FeedGeneratorService {
       <rss version="2.0">
         <channel>
           {{#each this}}
-            {{#if (eq @key "item")}}
-              {{#each this}}
-                <item>
-                  {{{renderItem this}}}
-                </item>
-              {{/each}}
-            {{else}}
               {{{renderNode @key this}}}
-            {{/if}}
           {{/each}}
         </channel>
       </rss>
@@ -34,6 +27,21 @@ export class FeedGeneratorService {
     `);
 
     Handlebars.registerHelper('renderNode', (key, value) => {
+      if (key === 'item') {
+        return value
+          .map((item: any) => {
+            return `<item>${Object.keys(item)
+              .map((itemKey) =>
+                Handlebars.helpers.renderNode(itemKey, item[itemKey]),
+              )
+              .join('')}</item>`;
+          })
+          .join('');
+      }
+      if (key === 'description') {
+        // 包装 description 内容为 CDATA
+        return `<${key}><![CDATA[${value}]]></${key}>`;
+      }
       if (typeof value === 'object' && !Array.isArray(value)) {
         let attributes = '';
         let content = '';
@@ -48,28 +56,7 @@ export class FeedGeneratorService {
             content += Handlebars.helpers.renderNode(subKey, subValue);
           }
         }
-        return `<${key} ${attributes}>${content}</${key}>`;
-      } else {
-        return `<${key}>${value}</${key}>`;
-      }
-    });
-
-    Handlebars.registerHelper('renderItem', (key, value) => {
-      if (typeof value === 'object' && !Array.isArray(value)) {
-        let attributes = '';
-        let content = '';
-        for (const [subKey, subValue] of Object.entries(value)) {
-          if (subKey === '$') {
-            attributes = Object.entries(subValue)
-              .map(([attrKey, attrValue]) => `${attrKey}="${attrValue}"`)
-              .join(' ');
-          } else if (subKey === '_') {
-            content = subValue as string;
-          } else {
-            content += Handlebars.helpers.renderItem(subKey, subValue);
-          }
-        }
-        return `<${key} ${attributes}>${content}</${key}>`;
+        return `<${key} ${attributes.trim()}>${content}</${key}>`;
       } else {
         return `<${key}>${value}</${key}>`;
       }
@@ -79,15 +66,21 @@ export class FeedGeneratorService {
       return a === b;
     });
   }
-
   generateFeed(jsonData: any, feedType: string): string {
+    let xmlString: string;
     if (feedType === 'rss2') {
-      return this.generateRssFeed(jsonData);
+      xmlString = this.generateRssFeed(jsonData);
     } else if (feedType === 'atom') {
-      return this.generateAtomFeed(jsonData);
+      xmlString = this.generateAtomFeed(jsonData);
     } else {
       throw new Error('Invalid feed type');
     }
+
+    return xmlFormat(xmlString, {
+      indentation: '  ', // 使用两个空格进行缩进
+      collapseContent: true, // 折叠内容
+      lineSeparator: '\n', // 换行符
+    });
   }
 
   private generateRssFeed(jsonData: any): string {
