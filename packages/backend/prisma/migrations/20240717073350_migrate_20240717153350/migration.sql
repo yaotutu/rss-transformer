3e9aa4be4ab358341c3f58edc25e5a10f6bc0073
yaotutu <957810668@qq.com>
2024-07-17 16:40:36 +0800

    feat: Split summarized content into multiple items in Summarized.tsx
    This commit updates the Summarized.tsx file to split the summarized content into multiple items based on the specified count. It introduces the partitionArray function to divide the summarized content array into smaller arrays of the specified size. The renderSummarizedContent function is modified to render each partitioned array separately, creating multiple items in the RSS feed. This change improves the organization and presentation of the summarized content.
    
    Co-authored-by: yaotutu

---
+++ packages/backend/prisma/migrations/20240717073350_migrate_20240717153350/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `itemDate` on the `RssTransformed` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_RssTransformed" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "rssItemId" INTEGER NOT NULL,
+    "taskId" INTEGER NOT NULL,
+    "uniqueArticleId" TEXT NOT NULL,
+    "itemUrl" TEXT NOT NULL,
+    "itemTransformedInfo" TEXT,
+    "feedType" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "RssTransformed_rssItemId_fkey" FOREIGN KEY ("rssItemId") REFERENCES "RssItem" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_RssTransformed" ("createdAt", "feedType", "id", "itemTransformedInfo", "itemUrl", "rssItemId", "taskId", "uniqueArticleId", "updatedAt") SELECT "createdAt", "feedType", "id", "itemTransformedInfo", "itemUrl", "rssItemId", "taskId", "uniqueArticleId", "updatedAt" FROM "RssTransformed";
+DROP TABLE "RssTransformed";
+ALTER TABLE "new_RssTransformed" RENAME TO "RssTransformed";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
